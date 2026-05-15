a3034d43dd05a8ab10335c48c58b9d8f75b57060
Jack Dawson <jack.dawson@uniteldirect.co.uk>
2026-05-15 14:20:02 +0100

    Fix importmap controller paths

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,11 @@
 import { application } from "controllers/application"
 
-import FlashController from "./flash_controller"
-import MeterReadingController from "./meter_reading_controller"
-import InvoiceBatchController from "./invoice_batch_controller"
-import VatController from "./vat_controller"
+import FlashController from "controllers/flash_controller"
+import MeterReadingController from "controllers/meter_reading_controller"
+import InvoiceBatchController from "controllers/invoice_batch_controller"
+import VatController from "controllers/vat_controller"
 
 application.register("flash", FlashController)
 application.register("meter-reading", MeterReadingController)
 application.register("invoice-batch", InvoiceBatchController)
-application.register("vat-controller", VatController)
+application.register("vat", VatController)
