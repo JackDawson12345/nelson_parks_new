1485c7ba93999fd9e9003cb7837bfe2972bb410a
Jack Dawson <jack.dawson@uniteldirect.co.uk>
2026-05-15 14:10:06 +0100

    wip

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,13 +1,11 @@
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
 import FlashController from "./flash_controller"
 import MeterReadingController from "./meter_reading_controller"
-import InvoiceBatchController from "./invoice_batch_controller";
-import VatController from "./vat_controller";
+import InvoiceBatchController from "./invoice_batch_controller"
+import VatController from "./vat_controller"
 
 application.register("flash", FlashController)
 application.register("meter-reading", MeterReadingController)
 application.register("invoice-batch", InvoiceBatchController)
 application.register("vat-controller", VatController)
-
-eagerLoadControllersFrom("controllers", application)
